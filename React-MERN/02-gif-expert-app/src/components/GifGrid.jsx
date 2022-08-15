88ee3dca9e1ff5d5e89cd218cc06d7b84b9bfe27
ZzzandyzzZ <andy_gean@hotmail.com>
2022-08-15 09:15:41 -0500

    Using loding flag to show data

--- React-MERN/02-gif-expert-app/src/components/GifGrid.jsx
+++ React-MERN/02-gif-expert-app/src/components/GifGrid.jsx
@@ -1,20 +1,17 @@
-import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { GifItem } from './GifItem';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({ category }) => {
-  // const [images, setImages] = useState([]);
-  // const getImages = async () => {
-  //   const newImages = await getGifs(category);
-  //   setImages(newImages);
-  // };
   const [images, isLoading] = useFetchGifs(category);
 
   return (
     <>
-      <h2>{ category }</h2>
+      <h3>{ category }</h3>
+      {
+        isLoading && (<h2>Cargando...</h2>)
+      }
       <div className="card-grid">
         {
           images.map(({ title, id, url }) => (
--- React-MERN/02-gif-expert-app/src/components/GifItem.jsx
+++ React-MERN/02-gif-expert-app/src/components/GifItem.jsx
@@ -1,14 +1,11 @@
 import PropTypes from 'prop-types';
 
-export const GifItem = ({ title, url }) => {
-  console.log();
-  return (
-    <div>
-      <img src={url} alt={title} />
-      <p>{ title }</p>
-    </div>
-  );
-};
+export const GifItem = ({ title, url }) => (
+  <div>
+    <img src={url} alt={title} />
+    <p>{ title }</p>
+  </div>
+);
 
 GifItem.propTypes = {
   title: PropTypes.string.isRequired,
