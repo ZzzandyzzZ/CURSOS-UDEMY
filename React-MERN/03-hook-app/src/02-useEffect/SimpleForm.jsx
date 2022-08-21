05b32d5f73ada463edc2e48a92ff612a218a432b
ZzzandyzzZ <andy_gean@hotmail.com>
2022-08-20 20:51:03 -0500

    CounterWithCustomHook component

---
+++ React-MERN/03-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    username: 'andy',
+    email: 'andy@gmail.com',
+  })
+  const {username, email} = formState;
+  const onInputChange = ({target}) => {
+    const {name, value} = target;
+    setFormState({
+      ...formState,
+      [name]:value,
+    })
+  }
+
+  return (
+    <>
+      <h1>Formulario Simple</h1>
+      <hr />
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Username"
+        name="username"
+        value={ username }
+        onChange={onInputChange}
+      />
+      <input
+        type="text"
+        className="form-control mt-2"
+        placeholder="email"
+        name="email"
+        value={ email }
+        onChange={onInputChange}
+      />
+    </>
+  )
+}
