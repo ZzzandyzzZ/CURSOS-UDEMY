f2d0f8c0d8001ee9990df8a9f840697a54586671
ZzzandyzzZ <andy_gean@hotmail.com>
2022-08-20 19:29:10 -0500

    Test to AddCategory component

--- React-MERN/02-gif-expert-app/tests/components/AddCategory.test.jsx
+++ React-MERN/02-gif-expert-app/tests/components/AddCategory.test.jsx
@@ -8,4 +8,25 @@ describe('Pruebas en <AddCategory />', () => {
     fireEvent.input(input, { target: { value: 'Goku' } });
     expect(input.value).toBe('Goku');
   });
+  test('Debe llamar onNewCategory si el input tiene un valor', () => {
+    const inputValue = 'Saitama';
+    const onNewCategory = jest.fn();
+    render(<AddCategory onNewCategory={onNewCategory} />);
+    const input = screen.getByRole('textbox');
+    const form = screen.getByRole('form');
+    fireEvent.input(input, { target: { value: inputValue } });
+    fireEvent.submit(form);
+    expect(input.value).toBe('');
+    expect(onNewCategory).toHaveBeenCalled();
+    expect(onNewCategory).toHaveBeenCalledTimes(1);
+    expect(onNewCategory).toHaveBeenCalledWith(inputValue);
+  });
+  test('No deberia llamar onNewCategory si el input esta vacio', () => {
+    const onNewCategory = jest.fn();
+    render(<AddCategory onNewCategory={onNewCategory} />);
+    const form = screen.getByRole('form');
+    fireEvent.submit(form);
+    expect(onNewCategory).not.toHaveBeenCalled();
+    expect(onNewCategory).toBeCalledTimes(0);
+  });
 });
