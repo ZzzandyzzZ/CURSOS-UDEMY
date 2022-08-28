9065ead49cb5620d8a412419952140fa0d51dd5c
ZzzandyzzZ <andy_gean@hotmail.com>
2022-08-28 18:49:50 -0500

    Add React Router

---
+++ React-MERN/03-hook-app/src/09-useContext/MainApp.jsx
@@ -0,0 +1,20 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { AboutPage } from './AboutPage'
+import { HomePage } from './HomePage'
+import { LoginPage } from './LoginPage'
+
+export const MainApp = () => {
+  return (
+    <>
+      <h1>MainApp</h1>
+      <hr />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="login" element={<LoginPage />} />
+        {/* <Route path="/*" element={<LoginPage />} /> */}
+        <Route path="/*" element={<Navigate to="/login" />} />
+      </Routes>
+    </>
+  )
+}
