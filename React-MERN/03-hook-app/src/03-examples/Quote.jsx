d3b587a5049fdc1c5d5b75c50134e0870236caec
ZzzandyzzZ <andy_gean@hotmail.com>
2022-08-29 18:01:40 -0500

    MultipleCustomHooks test

--- React-MERN/03-hook-app/src/03-examples/Quote.jsx
+++ React-MERN/03-hook-app/src/03-examples/Quote.jsx
@@ -14,7 +14,7 @@ export const Quote = ({author, quote}) => {
         style={{ display: 'flex' }}
       >
         <p ref={ pRef } className="mb-1">{ quote }</p>
-        <footer className="blockquote-footer">- { author }</footer>
+        <footer className="blockquote-footer">{ author }</footer>
       </blockquote>
       <code>
         { JSON.stringify(boxSize)}
