1716c845f419d2ba5a9451a1b7574e1238754680
ZzzandyzzZ <andy_gean@hotmail.com>
2022-09-04 11:01:19 -0500

    Create DcPage and Marvel Page

---
+++ React-MERN/04-heroes-spa/src/heroes/components/HeroList.jsx
@@ -0,0 +1,19 @@
+import { HeroCard } from '.';
+
+import { getHeroesByPublisher } from '../helpers'
+
+export const HeroList = ({publisher}) => {
+  const heroes = getHeroesByPublisher(publisher);
+  return (
+    <div className="row rows-cols-1 row-cols-md-3 g-2">
+      {
+        heroes.map(heroe => (
+          <HeroCard
+            key={heroe.id}
+            {...heroe}
+          />
+        ))
+      }
+    </div>
+  )
+}
