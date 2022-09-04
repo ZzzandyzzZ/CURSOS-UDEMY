c0c28ad831860b571e7301b1ed12b37bab18d4ce
ZzzandyzzZ <andy_gean@hotmail.com>
2022-09-04 15:49:04 -0500

    Add query string to parse url params

--- React-MERN/04-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ React-MERN/04-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,20 @@
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useForm } from '../../hooks/useForm'
 import { HeroCard } from '../components'
+import queryString from 'query-string'
 
 export const SearchPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const {q=''} = queryString.parse(location.search)
+  const {searchText, onInputChange} = useForm({
+    searchText: ''
+  })
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    if(searchText.trim().length <= 1) return;
+    navigate(`?q=${searchText.toLowerCase().trim()}`);
+  }
   return (
     <>
       <h1>Search</h1>
@@ -9,13 +23,15 @@ export const SearchPage = () => {
         <div className="col-5">
           <h4>Searching</h4>
           <hr />
-          <form action="">
+          <form onSubmit={onSearchSubmit}>
             <input
               type="text"
               placeholder="Search a hero"
               className="form-control"
               name="searchText"
               autoComplete="off"
+              value={searchText}
+              onChange={onInputChange}
             />
             <button className="btn btn-outline-primary mt-1">
               Search
@@ -29,7 +45,7 @@ export const SearchPage = () => {
             Select a hero
           </div>
           <div className="alert alert-danger">
-            No results with <b>ABC</b>
+            No results with <b>{ q }</b>
           </div>
           {/* <HeroCard /> */}
         </div>
