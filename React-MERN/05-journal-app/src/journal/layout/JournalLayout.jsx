ece57b99e89687f9afbfa4b0e6ce03a540a05c3b
ZzzandyzzZ <andy_gean@hotmail.com>
2022-10-01 12:49:56 -0500

    Create Navbar component

--- React-MERN/05-journal-app/src/journal/layout/JournalLayout.jsx
+++ React-MERN/05-journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,8 +1,12 @@
 import { Box } from '@mui/system'
+import { NavBar } from '../components'
+
+const drawerWidth = 240;
 
 export const JournalLayout = ({children}) => {
   return (
     <Box sx={{display: 'flex'}}>
+      <NavBar drawerWidth={drawerWidth}/>
       <Box
         component='main'
         sx={{flexGrow:1, p:3}}
