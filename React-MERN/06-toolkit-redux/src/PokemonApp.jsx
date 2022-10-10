e68cea0608b232f530b27dd9af5f50b59e5406cd
ZzzandyzzZ <andy_gean@hotmail.com>
2022-10-09 21:49:20 -0500

    Create pokemon thunks

--- React-MERN/06-toolkit-redux/src/PokemonApp.jsx
+++ React-MERN/06-toolkit-redux/src/PokemonApp.jsx
@@ -1,23 +1,29 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getPokemons } from './store/slices/pokemon/thunks';
 
 export const PokemonApp = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getPokemons())
-  
-    return () => {
-      
-    }
   }, [])
-  
+
+  const { isLoading, pokemons, page } = useSelector((state)=>state.pokemons)
   return (
     <>
       <h1>PokemonApp</h1>
+      <span>Loading: {isLoading ? 'Si' : 'No'}</span>
+      <hr />
       <ul>
-        <li>hola</li>
+        {
+          pokemons.map(({name})=>(
+            <li key={name}>{ name }</li>
+          ))
+        }
       </ul>
+      <button disabled={isLoading} onClick={()=>{dispatch(getPokemons(page))}}>
+        Next
+      </button>
     </>
   )
 }
