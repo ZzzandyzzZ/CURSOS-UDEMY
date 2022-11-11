24e8c3004ba4e4019876ce269218d2164e0967eb
ZzzandyzzZ <andy_gean@hotmail.com>
2022-11-10 22:00:01 -0500

    Check user auth

--- React-MERN/05-journal-app/src/router/AppRouter.jsx
+++ React-MERN/05-journal-app/src/router/AppRouter.jsx
@@ -1,11 +1,24 @@
-import { useSelector } from 'react-redux'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Route, Routes } from 'react-router-dom'
 import { AuthRoutes } from '../auth/routes/AuthRoutes'
+import { FirebaseAuth } from '../firebase/config'
 import { JournalRoutes } from '../journal/routes/JournalRoutes'
+import { login, logout } from '../store/auth'
 import { CheckingAuth } from '../ui'
 
 export const AppRouter = () => {
   const { status } = useSelector(state => state.auth)
+  const disptach = useDispatch();
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      if (!user) return disptach(logout());
+      const { uid, displayName, photoURL, email } = user;
+      disptach(login({uid, displayName, photoURL, email}))
+    });
+  }, [])
+
   if (status === 'checking') {
     return <CheckingAuth />
   }
