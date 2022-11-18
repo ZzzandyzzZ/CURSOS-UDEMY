0f817e3ff7a9a07a53cee21fc362067d10c49459
ZzzandyzzZ <andy_gean@hotmail.com>
2022-11-17 20:28:32 -0500

    Create notes in firestore

--- React-MERN/05-journal-app/src/store/journal/journalSlice.js
+++ React-MERN/05-journal-app/src/store/journal/journalSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 export const journalSlice = createSlice({
   name: 'journal',
   initialState: {
-    isSaving: true,
+    isSaving: false,
     messageSaved: '',
     notes: [],
     // active: {
@@ -14,11 +14,15 @@ export const journalSlice = createSlice({
     // },
   },
   reducers: {
-    addNewEmptyNote: (state) => {
-
+    savingNewNote: (state) => {
+      state.isSaving = true;
+    },
+    addNewEmptyNote: (state, action) => {
+      state.notes.push(action.payload);
+      state.isSaving = false;
     },
     setActiveNote: (state, action) => {
-
+      state.active = action.payload;
     },
     setNotes: (state, action) => {
 
@@ -42,4 +46,5 @@ export const {
   setSaving,
   updateNote,
   deleteNoteById,
+  savingNewNote,
 } = journalSlice.actions
