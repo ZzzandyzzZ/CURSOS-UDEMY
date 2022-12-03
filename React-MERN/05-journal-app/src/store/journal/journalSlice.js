8dbdc2df346fecc2bbf5a7811aa906689f3502ad
ZzzandyzzZ <andy_gean@hotmail.com>
2022-12-03 18:59:07 -0500

    Set alert message in store

--- React-MERN/05-journal-app/src/store/journal/journalSlice.js
+++ React-MERN/05-journal-app/src/store/journal/journalSlice.js
@@ -23,12 +23,14 @@ export const journalSlice = createSlice({
     },
     setActiveNote: (state, action) => {
       state.active = action.payload;
+      state.messageSaved = '';
     },
     setNotes: (state, action) => {
       state.notes = action.payload;
     },
     setSaving: (state, action) => {
       state.isSaving = true;
+      state.messageSaved = '';
     },
     updateNote: (state, action) => {
       state.isSaving = false;
@@ -37,7 +39,8 @@ export const journalSlice = createSlice({
           return action.payload;
         }
         return note;
-      }))
+      }));
+      state.messageSaved = `${action.payload.title} actualizado correctamente`;
     },
     deleteNoteById: (state, action) => {
 
