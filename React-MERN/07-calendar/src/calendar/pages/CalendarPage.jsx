69ed452adc0c943d63998a8bceda08896490fde1
ZzzandyzzZ <andy_gean@hotmail.com>
2023-02-26 21:43:10 -0500

    Setup big calendar react

--- React-MERN/07-calendar/src/calendar/pages/CalendarPage.jsx
+++ React-MERN/07-calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,43 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import format from 'date-fns/format';
+import parse from 'date-fns/parse';
+import startOfWeek from 'date-fns/startOfWeek';
+import getDay from 'date-fns/getDay';
+import enUS from 'date-fns/locale/en-US';
+import { addHours } from 'date-fns';
 import { Navbar } from '../components/Navbar';
+
+const locales = {
+  'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [{
+  title: 'Cumpleaños del jefe',
+  notes: 'Hay que comprar el paste',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+}];
 
 export function CalendarPage() {
   return (
     <>
       <Navbar />
-      <div />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+      />
     </>
   );
 }
