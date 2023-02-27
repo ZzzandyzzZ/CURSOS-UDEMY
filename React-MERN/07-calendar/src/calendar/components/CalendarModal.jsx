2c132883b74e970883df6ad00f4de579751291f5
ZzzandyzzZ <andy_gean@hotmail.com>
2023-02-26 21:43:10 -0500

    Create calendar modal

--- React-MERN/07-calendar/src/calendar/components/CalendarModal.jsx
+++ React-MERN/07-calendar/src/calendar/components/CalendarModal.jsx
@@ -28,7 +28,53 @@ export function CalendarModal() {
       overlayClassName="modal-fondo"
       closeTimeoutMS={200}
     >
-      <div>HOLA </div>
+      <h1> Nuevo evento </h1>
+      <hr />
+      <form className="container">
+        <div className="form-group mb-2">
+          <label htmlFor="start-date">
+            Fecha y hora inicio
+            <input className="form-control" placeholder="Fecha inicio" />
+          </label>
+        </div>
+        <div className="form-group mb-2">
+          <label htmlFor="end-date">
+            Fecha y hora fin
+            <input className="form-control" placeholder="Fecha inicio" />
+          </label>
+        </div>
+        <hr />
+        <div className="form-group mb-2">
+          <label htmlFor="event-title">
+            Titulo y notas
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Título del evento"
+              name="title"
+              autoComplete="off"
+            />
+          </label>
+          <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+        </div>
+        <div className="form-group mb-2">
+          <textarea
+            type="text"
+            className="form-control"
+            placeholder="Notas"
+            rows="5"
+            name="notes"
+          />
+          <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+        </div>
+        <button
+          type="submit"
+          className="btn btn-outline-primary btn-block"
+        >
+          <i className="far fa-save" />
+          <span> Guardar</span>
+        </button>
+      </form>
     </Modal>
   );
 }
