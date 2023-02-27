9359c64f62f6f2ecc08a42e8f8a9d0bd619cae69
ZzzandyzzZ <andy_gean@hotmail.com>
2023-02-26 21:43:11 -0500

    Add winston logger

--- React-MERN/08-calendar-back/index.js
+++ React-MERN/08-calendar-back/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const winston = require('winston');
+const expressWinston = require('express-winston');
+
 require('dotenv').config();
 
 const { dbConnection } = require('./database/config');
@@ -14,6 +17,21 @@ app.use(cors());
 app.use(express.static('public'));
 
 app.use(express.json())
+
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console()
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  ),
+  meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+  msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+  expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+  colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+  ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+}));
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
