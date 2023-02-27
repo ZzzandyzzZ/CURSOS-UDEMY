e2e8c6ae63661bfe29b6272219eb97b3c034ff7d
ZzzandyzzZ <andy_gean@hotmail.com>
2023-02-26 21:43:10 -0500

    Create Calendar page

--- React-MERN/07-calendar/src/main.jsx
+++ React-MERN/07-calendar/src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './styles.css'
-import { CalendarApp } from './CalendarApp'
+
 import { BrowserRouter } from 'react-router-dom'
+import { AppRouter } from './router/AppRouter'
+
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter >
-      <CalendarApp />
+      <AppRouter />
     </BrowserRouter>
   </React.StrictMode>,
 )
